8e8fad41967521500870a03c4f693c90fdc4665e
Surge1223 <surge1223@gmail.com>
2015-12-26 01:16:50 -0600

    Pointless, yet necessary name change

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -599,9 +599,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   #script.Print("Target: %s" % CalculateFingerprint(
   #    oem_props, oem_dict, OPTIONS.info_dict))
-  script.Print("********************")
-  script.Print("**   Pure NeXus   **")
-  script.Print("********************")
+  script.Print("******************************")
+  script.Print("** non-enforcing Pure NeXus **")
+  script.Print("******************************")
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
